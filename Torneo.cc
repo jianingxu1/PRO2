acddd3b6f3bb5eeb2e2de1598eed867268ca0829
xdarkroth <xdarkroth@gmail.com>
2022-04-26 20:35:54 +0200

    FIX Torneo::eliminar_puntos()
    When Torneo has not been played, the players have not got any points, so
    do nothing.

--- Torneo.cc
+++ Torneo.cc
@@ -48,9 +48,11 @@ Torneo::Torneo(int c) {
 }
 
 void Torneo::eliminar_puntos(Cjt_jugadores& jugadores_global) {
-    for (int i = 0; i < n; ++i) {
-        if (jugadores_edicion_actual[i].second != 0) {
-            jugadores_global.sumar_puntos_jugador(jugadores_edicion_actual[i].first, -jugadores_edicion_actual[i].second);
+    if (not primera_edicion and not iniciado) {
+        for (int i = 0; i < n; ++i) {
+            if (jugadores_edicion_anterior[i].second != 0) {
+                jugadores_global.sumar_puntos_jugador(jugadores_edicion_actual[i].first, -jugadores_edicion_actual[i].second);
+            }
         }
     }
 }
